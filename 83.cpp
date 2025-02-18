acedcff9961e0436929a0d03dc9f4f3aa18b0339
Rahul12886 <rahulrathore844124@gmail.com>
2025-02-18 09:59:44 +0530

    Create 83.cpp

---
+++ 83.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* current = head;
+        
+        while (current && current->next) {
+            if (current->val == current->next->val) {
+                current->next = current->next->next; // Skip the duplicate
+            } else {
+                current = current->next; // Move to the next node
+            }
+        }
+        
+        return head;
+    }
+};
+
+Input: [1,1,2]
+  Output: [1,2]
